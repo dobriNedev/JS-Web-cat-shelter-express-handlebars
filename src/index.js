59b8d7dab9bd6a7adb95641fcf9d63ff296ea06f
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-01-24 12:18:52 +0100

    added functionality for adding cat on the addCat view

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const formidable = require('formidable');
+const fs = require('fs');
+const path = require('path');
 //setup needs to be fixed
 //const config = require('./config/config.js'); 
 const db = require('./db.json');
+const Cat = require('./models/Cat');
 const port = 5001;
 
 const app = express();
@@ -14,7 +18,7 @@ app.set('views', './src/views');
 
 
 
-app.use(express.urlencoded({extended: false}));
+//app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     const cats = db.cats;
@@ -24,6 +28,53 @@ app.get('/', (req, res) => {
 app.get('/cats/add', (req, res) => {
     res.render('addCat');
 });
+
+// app.post('/cats/add', (req, res) => {
+//     let form = new formidable.IncomingForm();
+//     form.parse(req, (err, fields, files) => {
+//         if(err) {
+//             return console.error(err);
+//         }
+        
+//         const { name, breed, description } = fields;
+//         const imgUrl = '/images/cats/' + files.upload.originalFilename;
+//         console.log(`imgUrl: ${imgUrl}`);
+//         fs.rename(files.upload.filepath, imgUrl, (err) => {
+//             if (err) throw err;
+//             console.log('File moved to:', imgUrl);
+//         });
+//         let cat = new Cat(name, breed, imgUrl, description);
+//         cat.save();
+        
+//         res.redirect('/');
+//     });
+// });
+
+app.post('/cats/add', async (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.parse(req, async (err, fields, files) => {
+        if(err) {
+            return console.error(err);
+        }
+        console.log(`FilesUpload: ${files.upload.originalFilename}`);
+        const { name, breed, description } = fields;
+        //const imgUrl = path.relative(path.join(__dirname, '..'),path.join(__dirname,'public', 'images', 'cats', files.upload.originalFilename));
+        let imgUrl = '/images/cats/' + files.upload.originalFilename;
+        console.log(`imgUrl: ${imgUrl}`);
+        
+        fs.rename(files.upload.filepath, imgUrl, (err) => {
+            if (err) {
+                console.log(err)
+                
+            }
+            console.log('File moved to:', imgUrl);
+        });
+        let cat = new Cat(name, breed, imgUrl, description);
+        await cat.save();
+        res.redirect('/');
+    });
+});
+
 
 
 //we will use next line when the config setup is fixed!
