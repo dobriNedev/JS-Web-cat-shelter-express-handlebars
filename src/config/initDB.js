201d8eca85b1f851c146416cb407004a82d77f45
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-01-30 08:52:09 +0100

    created new config file for DB init

---
+++ src/config/initDB.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const config = require('./config');
+
+async function initDB() {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(config.DB_URI);
+    //in case we have a problem with the config.js we can use the connection string
+    await mongoose.connect('mongodb://localhost:27017/');
+    console.log('MongoDB is connected');
+}
+
+module.exports = initDB;
