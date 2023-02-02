000b5318077a93c02e36c0dcf9a45d4e18071668
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-02 14:51:57 +0100

    simple request data logger

---
+++ src/testRequest.js
@@ -0,0 +1,10 @@
+exports.testReq = (req) => {
+    const reqK = Object.keys(req);
+    console.log(`reqK:${ reqK }`)
+    const reqBodyK = Object.keys(req.body);
+    console.log(`reqBodyK:${ reqBodyK}`);
+    const reqParamsK = Object.keys(req.params);
+    console.log(`reqBodyK:${ reqParamsK}`);
+    const reqQueryK = Object.keys(req.query);
+    console.log(`reqBodyK:${ reqQueryK}`);
+}
