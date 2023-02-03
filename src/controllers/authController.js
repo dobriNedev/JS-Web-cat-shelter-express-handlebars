3530dcfbe63b8f63e6fd0b9846f1fd450e643467
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-03 15:49:41 +0100

    POST register with checking if username is unique

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,26 @@
-const User = require('../models/User');
-
+const authManager = require('../manager/authManager');
+const test = require('../testRequest');
 exports.getLogin = async(req, res) => {
     res.render('login');
 };
 
 exports.getRegister = async(req, res) => {
     res.render('register');
+};
+
+exports.postRegister = async(req, res) => {
+    const { firstName, lastName, username, email , password, repeatPassword } = req.body;
+    
+    if (password !== repeatPassword) {
+        res.status(404).send('<h2>Invalid username or password!</h2>').end();
+    }
+    const existingUser = await authManager.getUserByUsername(username);
+
+    if (existingUser) {
+        res.status(404).send('<h2>Invalid username or password!</h2>').end();
+    }
+
+    const user = await authManager.register(firstName, lastName, username, email , password);
+
+    res.redirect('login');
 };
