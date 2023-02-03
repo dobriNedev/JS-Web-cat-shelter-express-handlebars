c0701e9795b8c04e9af8c02dbb65ded584b4ded6
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-03 13:41:09 +0100

    added User model

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        minLength: 2,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minLength: 2,
+    },
+    email: {
+        type: String,
+        required: true,
+        match: [/[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g, 'Invalid email or password!']
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model('User', userSchema); 
+
+module.exports = User;
