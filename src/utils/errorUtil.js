33d50432f81eafb39399ce89c8261a0c76ad3f2e
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-20 09:33:36 +0100

    added error handling util

---
+++ src/utils/errorUtil.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+function getFirstMongooseError(error) {
+    const firstError = Object.values(error.errors)[0].message;
+    return firstError;
+}
+
+exports.getError = (error) => {
+    switch (error.name) {
+        case 'Error':
+            return error.message;
+        case 'ValidationError':
+            return getFirstMongooseError(error);
+        default:
+            return error.message;
+    }
+};
