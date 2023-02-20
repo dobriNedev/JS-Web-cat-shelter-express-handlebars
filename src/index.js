25519cea4b7229e52566f1773a4fbce133ec6e88
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-20 10:09:37 +0100

    added cookie-parser and global auth middleware to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const viewEngineSetup = require('./config/viewEngine');
 const routes = require('./routes')
+const { authenticate } = require('./middlewares/authMiddleware');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/config.js');
 const initDB = require('./config/initDB');
@@ -10,6 +12,8 @@ viewEngineSetup(app);
 
 app.use(express.static('src/public'));
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(authenticate);
 app.use(routes);
 
 initDB()
