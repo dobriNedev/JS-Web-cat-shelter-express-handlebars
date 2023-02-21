fae275b8d313804cde3634570152eedb2cfd1f4a
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-21 09:00:32 +0100

    added error messages and myShelteredCats

--- src/models/User.js
+++ src/models/User.js
@@ -3,22 +3,22 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: true,
-        minLength: 2,
+        required: [true, 'First name is required!'],
+        minLength: [2, 'First name is too short']
     },
     lastName: {
         type: String,
-        required: true,
-        minLength: 2,
+        required: [true, 'Last name is required!'],
+        minLength: [2, 'Last name is too short']
     },
     username: {
         type: String,
-        required: true,
-        minLength: 3
+        required: [true, 'Username is required!'],
+        minLength: [3, 'Username is too short']
     },
     email: {
         type: String,
-        required: true,
+        required: [true, 'Email is required!'],
         match: [/[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g, 'Invalid email or password!']
     },
     password: {
@@ -26,9 +26,14 @@ const userSchema = new mongoose.Schema({
         required: true,
         minLength: 3
     }, 
-    myCats: [ {type: mongoose.Types.ObjectId,
+    myOfferedCats: [ 
+        {type: mongoose.Types.ObjectId,
         ref: 'Cat'}
-    ]       
+    ], 
+    myShelteredCats: [ 
+        {type: mongoose.Types.ObjectId,
+        ref: 'Cat'}
+    ]    
 });
 
 const User = mongoose.model('User', userSchema); 
