66dace8dced2e5ebd56f2ac12dc25023364fcd4c
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-22 13:52:51 +0100

    GET profile functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -12,6 +12,7 @@ exports.postLogin = async(req, res) => {
     try {
         const token = await authManager.login(username, password);
         res.cookie(config.COOKIE_TOKEN_NAME, token);
+        
         res.redirect('/');
     } catch (error) {
         res.status(401).render('login', { error: getError(error) } );
@@ -34,3 +35,37 @@ exports.postRegister = async(req, res) => {
 };
 
 exports.getLogout = (req, res) => authManager.logout(req, res);
+
+exports.getProfile = async(req, res) => {
+    const username = res.locals.user?.username;
+    const isUser = Boolean(username);
+    
+    if (!isUser) {
+        throw new Error('Functionality ONLY for logged in users!');
+    }
+    try {
+
+        const user = await authManager.getUserByUsername(username).populate('myOfferedCats').populate('myShelteredCats').lean();
+    
+        const isOfferedCats = Boolean(user.myOfferedCats?.length);
+        
+        const isShelteredCats = Boolean(user.myShelteredCats?.length);
+        
+        let isCats = false;
+        if (isOfferedCats || isShelteredCats) {
+            isCats = true;
+        }
+        
+        const offeredCats = user.myOfferedCats;
+    
+        const shelteredCats = user.myShelteredCats;
+       
+        const offeredCount = user.myOfferedCats.length;
+        
+        const shelteredCount = user.myShelteredCats.length;
+
+        res.render('profile', { user, offeredCount, shelteredCount , isCats, isOfferedCats, offeredCats, isShelteredCats, shelteredCats });
+    } catch (error) {
+        res.status(400).render('index', { error: getError(error) } );
+    }
+};
