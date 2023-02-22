805cd0da0e38bf889ed92a1f00fbeff03f1db229
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-22 13:50:50 +0100

    added restrictions for owners and not owners by  guards/middleware

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Router = express.Router;
 const router = Router();
 const upload = require('./upload');
-const { isAuthenticated } = require('./middlewares/authMiddleware');
+const { isAuthenticated, isOwner, isNotOwner } = require('./middlewares/authMiddleware');
 //Controllers
 const homeController = require('./controllers/homeController');
 const breedController = require('./controllers/breedController');
@@ -21,18 +21,21 @@ router.get('/auth/register', authController.getRegister);
 router.post('/auth/register', authController.postRegister);
 //Logout
 router.get('/auth/logout', isAuthenticated, authController.getLogout);
+//Profile
+router.get('/auth/profile', isAuthenticated, authController.getProfile);
 //Cats
 //Add
 router.get('/cats/addCat',isAuthenticated, catController.getAddCat);
 router.post('/cats/addCat',isAuthenticated, upload.single('upload'), catController.postAddCat);
 //Edit
-router.get('/cats/:id/edit', isAuthenticated, catController.getEdit);
-router.post('/cats/:id/edit', isAuthenticated,  upload.single('image'), catController.postEdit);
+router.get('/cats/:id/edit', isAuthenticated, isOwner, catController.getEdit);
+router.post('/cats/:id/edit', isAuthenticated, isOwner , upload.single('image'), catController.postEdit);
 //Delete (aka Shelter)
-router.get('/cats/:id/shelterCat', isAuthenticated, catController.getShelterCat);
-router.post('/cats/:id/shelterCat', isAuthenticated, catController.postShelterCat);
+router.get('/cats/:id/shelterCat', isAuthenticated, isNotOwner, catController.getShelterCat);
+router.post('/cats/:id/shelterCat', isAuthenticated, isNotOwner, catController.postShelterCat);
 //Breeds
 router.get('/cats/addBreed',isAuthenticated, breedController.getBreedAdd);
 router.post('/cats/addBreed',isAuthenticated, breedController.postBreedAdd);
+
 
 module.exports = router;
